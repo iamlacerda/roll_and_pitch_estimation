72e93a0c9c4d9a4a406f47aed67e62c2fcf59304
Matheus Lacerda <matheus016@gmail.com>
2022-10-09 00:38:17 -0300

    added comments and another info on readme

--- README.md
+++ README.md
@@ -11,6 +11,35 @@ Having the elements as numeric values, it calculates the angles for Roll and Pit
 - Any IDE.
 - Your preferred bash.
 
+## Detail Description
+The program was made by following two methods.
+The first one was in a procedural way, used as a proof of concepts. Realizing at first all the logical groundrules for the working of the aplication.
+Hence, it was refactored within the guidelines of a Object Orientation Programming, by using the concept of Classes to achieve the expected main-goals.
+Moreover, it will detailed some points of the main logic, as it follows.
+
+- For acquire data of the log file:
+	It was used the library `fstream` both for input data e output data.
+	The function opens the file located in the same folder as the aplication itself.
+	Once it was open, the program follows the logic to parse the data.
+
+- For the parsing the raw data:
+	This function aggregates both the acquire and data treatment.
+	It collects the information, line by line, from the table data input, and stores it into a stream.
+	Further, the information of the string formated is read column by column, and it is stored at a position of a matrix.
+	The process is iterated, for all streamed values of the table data input as it becomes now a numeric variable, acessible for vectoring operations.
+
+- For the calculations:
+	By the method of Aerospace Rotation Sequence, the operation for determining the angles for the Pitch and Rolls is done iteratively.
+	As long as the matrix has lines, the calculation holds.
+	Once the values are calculated for every index, the associated value is placed in anothers vectors, for Pitch and Roll.
+
+- For the data-write into the outsource file:
+	Having utilized the library `fstream` again, the application creates a new file.
+	For the process of writing, a loop is generated for the all Pitch and Roll values are written down, as long as the size of the vector.
+	After writing, the application returns the message that a file is available in the Project folder.
+
+- More informations about the basics of the functions are shown in the code itself.
+
 ## How to Run it.
 Clone the Project folder by doing (or download it from GitHub):
 ```
@@ -35,7 +64,11 @@ It will have the time stamps, alongside with the estimated angles for Pitch and 
 
 ## Known Bugs
 It will not overwrite the table generated. For a new visualization it is necessary to delete the Roll_and_Pitch_Estimations.txt archive in Project folder, and run the application again.
-
+It will show a error message if the 
+```
+#define _CRT_SECURE_NO_WARNINGS 
+```
+is not able. It is demanding for timing purposes.
 ## License
 
 Matheus Lacerda, 2022.
--- Roll_and_Pitch_Estimator_aerospace_rotation_sequence.cpp
+++ Roll_and_Pitch_Estimator_aerospace_rotation_sequence.cpp
@@ -1,22 +1,25 @@
-#define _USE_MATH_DEFINES
-#define _CRT_SECURE_NO_WARNINGS
+#define _USE_MATH_DEFINES // For using the most precise value of PI.
+#define _CRT_SECURE_NO_WARNINGS // Necessary to run the time applications. Compiler indicates a error without using it and recommends the usage.
 
 #include <iostream> 
-#include <cmath> 
-#include <fstream>
-#include <string>
+#include <cmath> //Library for the mathematical calculations.
+#include <fstream> //For operations with outsource files.
+#include <string> //Operations with strings.
 #include <sstream>
 #include <cstdio>
-#include <chrono>
-#include <ctime>
+#include <chrono> //Using with the ctime library to generate date and time.
+#include <ctime> //Using with the chrono library to generate date and time.
 
 
 using namespace std;
 
-#define INPUT_FILE "attitude_exam.log"
+#define INPUT_FILE "attitude_exam.log" 
 #define OUTPUT_FILE "Roll_and_Pitch_Estimation.txt"
 
-class Desafio
+/*
+This class is responsible for read data, parse it, calculate and write it down.
+*/
+class Desafio 
 {
 public:
 
@@ -29,23 +32,23 @@ public:
 	}
 
 private:
-
+	
 	int line = 0, column = 0;
 	float matrice[5500][4];
 	float roll[5500];
 	float pitch[5500];
 
 
-	void read_input()
+	void read_input() //This function reads all the data and stores it into a matrix.
 	{
 		tic_tac("Starting to read the data input.");
 		string line_content, column_content;
 		ifstream log_file(INPUT_FILE);
-		while (getline(log_file, line_content))
+		while (getline(log_file, line_content)) //Get each line.
 		{
 			stringstream line_stream(line_content);
 			column = 0;
-			while (getline(line_stream, column_content, ';'))
+			while (getline(line_stream, column_content, ';')) //Get each column.
 			{
 				matrice[line][column] = clean_column(column_content);
 				column++;
@@ -57,21 +60,25 @@ private:
 
 	}
 
-
-	void estimate_angle()
+	/*
+	For each element of the matrix contening the variation of the axis,
+	it performs a mathematical operation to set the roll and pitch angles.
+	*/
+	void estimate_angle() 
 	{
 		int j;
 		for (j = 0; j < line; j++)
 		{
 			roll[j] = estimate_roll(matrice[j][2], matrice[j][3]);
 			pitch[j] = estimate_pitch(matrice[j][1], matrice[j][2], matrice[j][3]);
-			//cout << j << "- pitch:" << pitch[j] << " | roll:" << roll[j] << endl;
 		}
 		cout << "Number of iterations: " << j << endl;
 		tic_tac("Calculations on Pitch and Roll Estimated Angles Completed and starting to Elaborate the data Output..");
 	}
 
-
+	/*
+	Write the elements into a output file.
+	*/
 	void write_output()
 	{
 		int k;
@@ -86,19 +93,25 @@ private:
 		tic_tac("The data Output for the Pitch and Roll Estimated angles are now available in the file 'Roll_and_Pitch_Estimation.txt'.");
 	}
 
-
+	/*
+	Calculation of the Pitch angle, given the axis variation.
+	*/
 	float estimate_pitch(float accel_x_axis, float accel_y_axis, float accel_z_axis)
 	{
 		return 180 * (atan(accel_x_axis / (sqrt((accel_y_axis * accel_y_axis) + (accel_z_axis * accel_z_axis))))) / M_PI;
 	}
 
-
+	/*
+	Calculation of the Roll angle, given the axis variation.
+	*/
 	float estimate_roll(float accel_y_axis, float accel_z_axis)
 	{
 		return 180 * (atan(accel_y_axis / accel_z_axis)) / M_PI;
 	}
 
-
+	/*
+	Trim function. Return data with no blank spaces or commas.
+	*/
 	float clean_column(string data_column)
 	{
 		float i;
@@ -106,6 +119,9 @@ private:
 		return i;
 	}
 
+	/*
+	Display a custom message with the currente time.
+	*/
 	void tic_tac(string message)
 	{
 		auto message_time = chrono::system_clock::now();
@@ -117,6 +133,6 @@ private:
 
 int main()
 {
-	Desafio task;
-	task.resolve();
+	Desafio task; //Initialize the task object.
+	task.resolve(); //Call the resolution method.
 }
